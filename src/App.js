b25ea8298f0eabbcad498b3d819a552214175735
Seda Malkoç <sedaamalkocc@gmail.com>
2024-02-10 21:45:47 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [randomColor, setRandomColor] = useState("#FFFFFF");
+
+  const generateRandomColor = () => {
+    const randomColorCode =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setRandomColor(randomColorCode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Rastgele Renk Üretici</h1>
+      <div
+        className="container"
+        onClick={generateRandomColor}
+        style={{
+          width: "200px",
+          height: "200px",
+          backgroundColor: randomColor,
+          cursor: "pointer",
+        }}
+      >
+        <button>Tıkla ve rengi değiştir!</button>
+      </div>
     </div>
   );
 }
